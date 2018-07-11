a2f18d3b801e69438d970de3f08702369c258eb3
Dirk Rademann <dirk.rademann@lhind.dlh.de>
2018-07-11 17:54:53 +0200

    Fixes test.

--- src/prime-factors.ts
+++ src/prime-factors.ts
@@ -1,7 +1,11 @@
-export function primeFactors(number) {
+export function primeFactors(number: number) {
     let factors: number[] = [];
+    if (number % 2 == 0) {
+        factors = [...factors, 2];
+        number /= 2
+    }
     if (number > 1) {
-        factors = factors.concat(number)
+        factors = [...factors, number]
     }
     return factors;
 }
