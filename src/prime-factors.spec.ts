0925397f5d728c4cea1f7d05d8facead510b7c6f
Dirk Rademann <dirk.rademann@lhind.dlh.de>
2018-07-12 10:37:21 +0200

    Introduces formatting by 'Prettier' (www.prettier.io).
    Hint: Use FileWatcher in WebStorm:
    https://prettier.io/docs/en/webstorm.html#running-prettier-on-save-using-file-watcher

--- src/prime-factors.spec.ts
+++ src/prime-factors.spec.ts
@@ -1,19 +1,19 @@
-import {primeFactors} from './prime-factors'
+import { primeFactors } from "./prime-factors";
 
-import {expect} from 'chai';
-import 'mocha'
+import { expect } from "chai";
+import "mocha";
 
-describe('calculating primefactors of a natural number', () => {
-    it('should return an empty array given number 1', () => {
-        expect(primeFactors(1)).to.be.empty
-    });
-    it('should return [2] given number 2', () => {
-        expect(primeFactors(2)).to.deep.equal([2]);
-    });
-    it('should return [3] given number 3', () => {
-        expect(primeFactors(3)).to.deep.equal([3]);
-    });
-    it('should return [2, 2} given number 4', () => {
-        expect(primeFactors(4)).to.deep.equal([2, 2]);
-    });
+describe("calculating primefactors of a natural number", () => {
+  it("should return an empty array given number 1", () => {
+    expect(primeFactors(1)).to.be.empty;
+  });
+  it("should return [2] given number 2", () => {
+    expect(primeFactors(2)).to.deep.equal([2]);
+  });
+  it("should return [3] given number 3", () => {
+    expect(primeFactors(3)).to.deep.equal([3]);
+  });
+  it("should return [2, 2} given number 4", () => {
+    expect(primeFactors(4)).to.deep.equal([2, 2]);
+  });
 });
--- src/prime-factors.ts
+++ src/prime-factors.ts
@@ -1,11 +1,11 @@
 export function primeFactors(number: number) {
-    let factors: number[] = [];
-    if (number % 2 == 0) {
-        factors = factors.concat(2);
-        number /= 2
-    }
-    if (number > 1) {
-        factors = factors.concat(number)
-    }
-    return factors;
+  let factors: number[] = [];
+  if (number % 2 == 0) {
+    factors = factors.concat(2);
+    number /= 2;
+  }
+  if (number > 1) {
+    factors = factors.concat(number);
+  }
+  return factors;
 }
