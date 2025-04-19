bd2d2b6b6c4c3078270ae67d6a075e8ea95183bf
Dirk Rademann <dirk.rademann@icloud.com>
2025-04-19 14:48:09 +0200

    refactors example to playground

--- src/example.test.ts
+++
@@ -1,7 +0,0 @@
-import { plus } from './example';
-
-describe('plus should', () => {
-  test('calculate the sum of two numbers', () => {
-    expect(plus(2, 3)).toBe(5);
-  });
-});
---
+++ src/playground.test.ts
@@ -0,0 +1,3 @@
+describe('playground for', () => {
+  test('something', () => {});
+});
